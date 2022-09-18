d530f5c30d272c91396b8663dd9418a8e7cf9ede
CinYP <111353883+CinYP@users.noreply.github.com>
2022-09-18 18:24:58 -0400

    JavaScript & HTML Code Update

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,55 @@
 // Assignment Code
+/*let numeValue = document.querySelector(#charactersreqd);
+let caseLower = document.querySelector(#lowerCase);
+let caseUpper = document.querySelector(#upperCase);
+let specialChact = document.querySelector(#characterSpecial);
+let numericVl = document.querySelector(#characterNumeric);
+let pwRequrements = document.querySelectorAll(".pw-parameters"); */
+
+var passwordInputs = {
+  numeValue = document.querySelector(#charactersreqd);
+
+  caseLower = document.querySelector(#lowerCase);
+
+  caseUpper = document.querySelector(#upperCase);
+
+  specialChact = document.querySelector(#characterSpecial);
+  
+  numericVl = document.querySelector(#characterNumeric);
+
+  pwRequrements = document.querySelectorAll(".pw-parameters");
+}
+
+function numeValue() {
+  var result           = '';
+  var characters       = '0123456789';
+  charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return;
+}
+
+numeValue();
+
+console.log()
+
+function caseLower(length) {
+  var result           = '';
+  var characters       = 'abcdefghijklmnopqrstuvwxyz';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return;
+}
+
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -9,6 +60,15 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+//addEventListener('input', (event) =>{});
+//oninput = (event) => { };
+
+pwRequrements.forEach('input', (event) => {};addEventListener(click,() => {
+ writePassword()
+
+  let randNum = Math.floor(Math.random)
+} )); 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
