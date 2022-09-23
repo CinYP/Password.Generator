9404b44c7e91e484ae4011fa087081973f317cfc
CinYP <111353883+CinYP@users.noreply.github.com>
2022-09-22 21:44:46 -0400

    Refreshing JavaScript code with additional functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,74 +1,103 @@
-// Assignment Code
-/*let numeValue = document.querySelector(#charactersreqd);
-let caseLower = document.querySelector(#lowerCase);
-let caseUpper = document.querySelector(#upperCase);
-let specialChact = document.querySelector(#characterSpecial);
-let numericVl = document.querySelector(#characterNumeric);
-let pwRequrements = document.querySelectorAll(".pw-parameters"); */
 
-var passwordInputs = {
-  numeValue = document.querySelector(#charactersreqd);
-
-  caseLower = document.querySelector(#lowerCase);
-
-  caseUpper = document.querySelector(#upperCase);
-
-  specialChact = document.querySelector(#characterSpecial);
-  
-  numericVl = document.querySelector(#characterNumeric);
-
-  pwRequrements = document.querySelectorAll(".pw-parameters");
-}
-
-function numeValue() {
-  var result           = '';
-  var characters       = '0123456789';
-  charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * 
-charactersLength));
- }
- return;
-}
-
-numeValue();
-
-console.log()
-
-function caseLower(length) {
-  var result           = '';
-  var characters       = 'abcdefghijklmnopqrstuvwxyz';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * 
-charactersLength));
- }
- return;
-}
+//DOM Element declaration
+const resultElement = document.getElementById('password');
+const lengthElement = document.getElementById('number');
+const lowerCaseElement = document.getElementById('lowerCase');
+const upperCaseElement = document.getElementById('upperCase');
+const characterSpecialElement = document.getElementById('characterSpecial');
+const characterNumericElement = document.getElementById('characterNumeric');
+const generateBtn = document.getElementById('generate');
+// starter code provided: var generateBtn = document.querySelector("#generate");
 
 
 
+//greating an object for the variables that will create the password
+  let randomFunctions = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol,
+  }
+  //creating functions for reach of the selected options. 
+let lowerchar = "abcdefghijklmnopqrstuvwxyz"
+let upperchar= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let numberchar = "123456789"
+let symbolchar = "!@#$%^&*()_-+=?.,<>;:"
 
 
-var generateBtn = document.querySelector("#generate");
+//generating functions to randomly assign a character to the length later on 
+  function getRandomLower(){
+    return String.fromCharCode(Math.floor(Math.random()* 26)+ 97 )
+    }
 
+  function getRandomUpper(){
+      return String.fromCharCode(Math.floor(Math.random()*26) + 65)
+    }
+    
+  function getRandomNumber(){
+      return String.fromCharCode(Math.floor(Math.random()*10)+48)
+    }
+    
+  function getRandomSymbol() {
+      let symbols  = '!@#$%^&*()_-+=/{}[]~`';
+      for ( var i = 0; i < symbols.length; i++ ) {
+        return symbols.charAt(Math.floor(Math.random() * 
+    symbols.length));
+     }
+  }
+
+// Generate Event Listener 
+generateBtn.addEventListener('click', () => {
+  let length = lengthElement.value;
+  let clickLower = lowerCaseElement.checked;
+  let clickUpper = upperCaseElement.checked;
+  let clickNumb =  characterNumericElement.checked;
+  let clicksymbol = characterSpecialElement.checked;
+
+
+  resultElement.innerText = generatePassword(clickLower, clickNumb, clickUpper, clicksymbol,length);
+// the innerText property of the HTMLElement interface represents the rendered text - A string representing the rendered text content of an element.
+//this should return the text to the screen?
+
+});
+
+// Password function 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword (lower, upper, number, symbol, length) {
+  //creating initial password variables
+  //variable to generate passowrd and we're setting this equal to an empty string 
+console.log(lower, upper, number, symbol);
 
-  passwordText.value = password;
+let potentialPassword = "";
+let generatedPassword = "";
 
+if (lower){
+  let singlelowerchar = randomFunctions.lower();
+  potentialPassword += lowerchar
+} 
+if (upper){
+  let singleupperchar = randomFunctions.upper();
+  potentialPassword += upperchar
+}
+if (symbol){
+  let singlesymbolchar = randomFunctions.symbol()
+  potentialPassword += symbolchar
+} 
+if (number){
+  let singlelowerchar = randomFunctions.number();
+  potentialPassword += numberchar
+} 
+
+//creating a for-loop for selected buttons and choices, that will generate password
+for (i = 0; i <length; i++) {
+  var randomindexpassword = Math.floor(Math.random()* potentialPassword.length)
+  var randomcharacter = potentialPassword[randomindexpassword]
+console.log(randomindexpassword, randomcharacter);
+  generatedPassword += randomcharacter
+}
+console.log(generatedPassword);
+
+return generatedPassword;
 }
 
-//addEventListener('input', (event) =>{});
-//oninput = (event) => { };
 
-pwRequrements.forEach('input', (event) => {};addEventListener(click,() => {
- writePassword()
-
-  let randNum = Math.floor(Math.random)
-} )); 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
